ebaede219b565f645f47bf78b68e038b563152ed
Martyn Janes <martyn@obany.com>
2020-06-08 16:59:15 +0100

    Update query server to express

--- examples/queryServer/components/zmq.js
+++ examples/queryServer/components/zmq.js
@@ -4,24 +4,28 @@ const { storeTransaction } = require('./db')
 let zmq = require('zeromq')
 let sock = zmq.socket('sub')
 
-// Connect to the devnet node's ZMQ port
-console.log('Connecting to ZMQ')
-sock.connect(process.env.ZMQ_URL)
+function startZMQ() {
+  // Connect to the devnet node's ZMQ port
+  console.log('Connecting to ZMQ')
+  sock.connect(process.env.ZMQ_URL)
 
-sock.subscribe('trytes')
+  sock.subscribe('trytes')
 
-sock.on('message', async msg => {
-  const data = msg.toString().split(' ') // Split to get topic & data
-  const tx_id = data[2] // TX Hash
-  const tag = data[1].slice(2592, 2619) // Extract tag
-  const iac = extract(tag) // Check that the tag is correct
+  sock.on('message', async msg => {
+    const data = msg.toString().split(' ') // Split to get topic & data
+    const tx_id = data[2] // TX Hash
+    const tag = data[1].slice(2592, 2619) // Extract tag
+    const iac = extract(tag) // Check that the tag is correct
 
-  // Qualify TAG
-  if (iac) {
-    console.log('IAC:', iac)
-    console.log('Tx Hash:', tx_id)
-    await storeTransaction([{ tx_id, iac }])
-  }
-})
+    // Qualify TAG
+    if (iac) {
+      console.log('IAC:', iac)
+      console.log('Tx Hash:', tx_id)
+      await storeTransaction([{ tx_id, iac }])
+    }
+  })
+}
 
-module.exports = {}
+module.exports = {
+  startZMQ
+}
